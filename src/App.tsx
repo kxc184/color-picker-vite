734ecfd2dd66124321f2e2275768e085f97b7292
Kyle Corcoran <Kyle.Corcoran@sherwin.com>
2025-05-20 00:20:59 -0400

    remove solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,24 @@
-import { useEffect, useState } from "react";
-import ColorChip from "./components/ColorChip";
-import { findClosestColors } from "./utils";
-import type { ColorMatch } from "./types/color";
-import ColorPicker from "./components/ColorPicker";
+import { useEffect } from "react";
 
 function App() {
-  const [color, setColor] = useState("#000000");
-  const [matches, setMatches] = useState<ColorMatch[]>([]);
+  // init color, matches states
 
   useEffect(() => {
     const fetchClosestColors = async () => {
-      const result = await findClosestColors(color);
-      setMatches(result);
+      // use findClosestColors from utils to do math stuff and find top three closest colors
+      // to sherwin williams colors
+      // then updated your matches state
     };
     fetchClosestColors();
-  }, [color]);
+  }, []);
 
   return (
     <main className=" flex flex-col items-center justify-center w-screen h-screen gap-8 bg-gray-300">
-      <ColorPicker color={color} onChange={setColor} />
-      <div className="flex  gap-2">
-        {matches.map((c, i) => (
-          <ColorChip key={i} color={c.hex} name={c.name} />
-        ))}
-      </div>
+      <p>Use both color picker component to choose color</p>
+      <p>
+        Use color chips to display top 3 Colors closest to the color you
+        selected
+      </p>
     </main>
   );
 }
