0147ecb88ebb2586cbca516d42b2fb235cb73bb2
Kyle Corcoran <Kyle.Corcoran@sherwin.com>
2025-05-20 08:52:43 -0400

    add more refined styles

--- src/components/ColorChip.tsx
+++ src/components/ColorChip.tsx
@@ -1,11 +1,14 @@
 const ColorChip = ({ color, name }: { color: string; name: string }) => {
   return (
-    <div className="rounded-md shadow-xl border border-black/40  w-32 h-44 p-2">
+    <div className="rounded-md  shadow-inner border border-gray-100 w-32 h-44 p-2 bg-white transition-transform hover:scale-105 duration-300">
       <div className="flex flex-col h-full">
-        <div className="w-full h-32" style={{ backgroundColor: color }}></div>
-        <div className="flex flex-col h-full justify-around ">
-          <p className=" text-lg">{name}</p>
-          <p className="text-sm text-gray-600">{color}</p>
+        <div
+          className="w-full h-32 rounded-md shadow-inner"
+          style={{ backgroundColor: color }}
+        ></div>
+        <div className="flex flex-col h-full justify-around mt-2">
+          <p className="text-lg font-semibold">{name}</p>
+          <p className="text-sm text-gray-600 tracking-wide">{color}</p>
         </div>
       </div>
     </div>
